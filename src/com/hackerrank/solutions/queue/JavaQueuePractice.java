66f5a1af7a712d7bd4909ae77c4ec5d795391f57
Cameron Moore <cmoore@captechventures.com>
2018-08-29 16:05:42 -0400

    Made progress on queue exercise

--- src/com/hackerrank/solutions/queue/JavaQueuePractice.java
+++ src/com/hackerrank/solutions/queue/JavaQueuePractice.java
@@ -1,33 +1,52 @@
 package com.hackerrank.solutions.queue;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class JavaQueuePractice {
-	//queue can be added or removed to either front or back
-	//How to implement them:
-			//LinkedList<>
-			// ArrayDeque<>
-		
-		//Goal: find maximum number of unique integers among all the possible subarrays of size M
-		
-		//inputs: 6 3 : 2 unique integers
-		//inputs 535232 : 3 unique integers
-		
-		
-		//idea: Create a queue that looks through each subarray and finds the unique integers
-		//combine the data from doing that into an answer
-		//return an int
-		//TO DO: Look up common methods with dequeue
+	// queue can be added or removed to either front or back
+	// How to implement them:
+	// LinkedList<>
+	// ArrayDeque<>
 
-		public List<Integer> countUniqueNumbers(Set<Set<Integer>> inputValues) {
-			Deque<Integer> deque = new LinkedList<>();
-			for(Set<Integer> set: inputValues);
+	// Goal: find maximum number of unique integers among all the possible subarrays
+	// of size M
+
+	// inputs: 6 3 : 2 unique integers
+	// inputs 535232 : 3 unique integers
+
+	// idea: Create a queue that looks through each subarray and finds the unique
+	// integers
+	// combine the data from doing that into an answer
+	// return an int
+	// TO DO: Look up common methods with dequeue
+
+	public List<Integer> countUniqueNumbers(Set<ArrayList<Integer>> inputValues) {
+		int count = 0;
+		Map<Integer, Integer> countOccurences = new HashMap<>();
+		Deque<Integer> deque = new ArrayDeque<Integer>();
+		for (List<Integer> set : inputValues) {
+			deque.addAll(set);
+			for (int i : deque) {
 				
-			
-			return null;
+				if (!countOccurences.containsValue(i)) {
+					countOccurences.put(i,1);
+				} else {
+					int value = countOccurences.get(i);
+					value++;
+					
+				}
+
+			}
+
 		}
+
+		return null;
+	}
 }
--- test/com/hackerrank/solutions/testQueue/JavaQueuePracticeTest.java
+++ test/com/hackerrank/solutions/testQueue/JavaQueuePracticeTest.java
@@ -14,15 +14,17 @@ import org.junit.Test;
 
 public class JavaQueuePracticeTest {
 	Set<List<Integer>> setOfIntegerLists = new HashSet<List<Integer>>();
-	List<Integer> list1 = new ArrayList<>(Arrays.asList(6,3,7,7,8,9,2,8));
-	List<Integer> list2 = new ArrayList<>(Arrays.asList(5,3,5,2,3,3));	@Before
+	List<Integer> list1 = new ArrayList<>(Arrays.asList(6, 3, 7, 7, 8, 9, 2, 8));
+	List<Integer> list2 = new ArrayList<>(Arrays.asList(5, 3, 5, 2, 3, 3));
+
+	@Before
 	public void setup() {
-	setOfIntegerLists.add(list1);
-	setOfIntegerLists.add(list2);
+		setOfIntegerLists.add(list1);
+		setOfIntegerLists.add(list2);
 	}
-	
+
 	@Test
 	public void testQueueClass() {
-		
+
 	}
 }

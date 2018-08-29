f1cc820b31c041e2fbbc0247c8df8ddd4eff71a9
Cameron Moore <cmoore@captechventures.com>
2018-08-29 14:59:40 -0400

    Setup class and test class for Queuing exercise

--- src/com/hackerrank/solutions/queue/JavaQueuePractice.java
+++ src/com/hackerrank/solutions/queue/JavaQueuePractice.java
@@ -1,5 +1,33 @@
 package com.hackerrank.solutions.queue;
 
-public class JavaQueuePractice {
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
+public class JavaQueuePractice {
+	//queue can be added or removed to either front or back
+	//How to implement them:
+			//LinkedList<>
+			// ArrayDeque<>
+		
+		//Goal: find maximum number of unique integers among all the possible subarrays of size M
+		
+		//inputs: 6 3 : 2 unique integers
+		//inputs 535232 : 3 unique integers
+		
+		
+		//idea: Create a queue that looks through each subarray and finds the unique integers
+		//combine the data from doing that into an answer
+		//return an int
+		//TO DO: Look up common methods with dequeue
+
+		public List<Integer> countUniqueNumbers(Set<Set<Integer>> inputValues) {
+			Deque<Integer> deque = new LinkedList<>();
+			for(Set<Integer> set: inputValues);
+				
+			
+			return null;
+		}
 }
--- test/com/hackerrank/solutions/testQueue/JavaQueuePracticeTest.java
+++ test/com/hackerrank/solutions/testQueue/JavaQueuePracticeTest.java
@@ -1,5 +1,28 @@
 package com.hackerrank.solutions.testQueue;
 
-public class JavaQueuePracticeTest {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class JavaQueuePracticeTest {
+	Set<List<Integer>> setOfIntegerLists = new HashSet<List<Integer>>();
+	List<Integer> list1 = new ArrayList<>(Arrays.asList(6,3,7,7,8,9,2,8));
+	List<Integer> list2 = new ArrayList<>(Arrays.asList(5,3,5,2,3,3));	@Before
+	public void setup() {
+	setOfIntegerLists.add(list1);
+	setOfIntegerLists.add(list2);
+	}
+	
+	@Test
+	public void testQueueClass() {
+		
+	}
 }
